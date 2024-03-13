97229a55b0ea5e53d3626e8809f1493bcda905d1
Moriba Hebelamou <dev.hebelamou@gmail.com>
2024-03-13 01:01:52 +0000

    init project

---
+++ src/main/java/com/trustify/darktransfertdata/DarktransfertdataApplication.java
@@ -0,0 +1,13 @@
+package com.trustify.darktransfertdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarktransfertdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarktransfertdataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trustify/darktransfertdata/DarktransfertdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustify.darktransfertdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarktransfertdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
